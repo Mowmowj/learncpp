e7e1475e421ab05a0d75b26490b25746c882bf97
shelton cheung <sheltonzhang@xiaoe-tech.com>
2020-12-18 10:22:14 +0800

    Initial Commit

---
+++ learnCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  learnCpp
+//
+//  Created by Shelton Cheung on 2020/12/18.
+//  Copyright © 2020 xiaoetong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
